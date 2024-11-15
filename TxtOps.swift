da829d3efc35e93ff876485814297024e797af0a
DiegoAraaujo <diegoaraaujo8@gmail.com>
2024-11-15 13:52:34 -0300

    Refatoração da função main para melhorar a validação de entrada

--- TxtOps.swift
+++ TxtOps.swift
@@ -1,28 +1,26 @@
 import Foundation
 
 print("----------------------Bem-vindo ao TextCLI!-------------------------")
-print("serviços disponíveis:")
-print("| 1 = Contar palavras | 2 =  Buscar palavra | 3 = Substituir palavra |")
+print("Serviços disponíveis:")
+print("| 1 = Contar palavras | 2 = Buscar palavra | 3 = Substituir palavra |")
 
 func main() {
-    let servico = readLine() 
+    print("Digite o número do serviço desejado:")
+    
+    guard let servico = readLine(), !servico.isEmpty else {
+        print("ERRO: Não foi possível ler a entrada. Por favor, digite uma opção válida.")
+        return
+    }
 
-    if servico != nil {  
-        let comando = servico!  
-        switch comando {
-        case "1":
-            //contarPalavras()
-        case "2":
-            //buscarpalavras()
-        case "3":
-            //alterarpalavras()
-        default:
-            print("ERRO: Você deve digitar uma das opções acima!")
-        }
-    } else {
-        print("ERRO: Não foi possível ler a entrada.")
+    switch servico {
+    case "1":
+        contarPalavras()
+    case "2":
+        searchWord()
+    case "3":
+        replaceWord()
+    default:
+        print("ERRO: Você deve digitar uma das opções acima!")
     }
 }
-
-// Chamando a função principal
 main()
